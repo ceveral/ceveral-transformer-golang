68467f0f6bbd6544f8f8c54ab0d72c1fb0941dc7
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2017-02-04 14:01:15 +0100

    added annotation declarations

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,34 @@ import {GolangVisitor} from './visitor'
 
 export default {
 	name: 'Golang',
+	annotations: {
+        records: {
+            gotags: {
+                arguments: '[string]|string',
+                description: "Generate struct tags on all fields"
+            },
+            doc: {
+                arguments: "string",
+                description: "Generate documenting comments"
+            }
+        },
+        properties: {
+            gotags: {
+                arguments: '[string]|{string}'
+            },
+            gopointer: {
+                arguments: "boolean",
+                description: "Declare the field as a pointer"
+            },
+            gotype: {
+                arguments: 'string',
+                description: "Override Go type"
+            },
+            doc: {
+                arguments: "string"
+            }
+        }
+    },
 	transform(ast: ImportedPackageExpression, options:TranspileOptions): Promise<IResult[]> {
 		let visitor = new GolangVisitor(options);
 		return Promise.resolve(visitor.parse(ast));
